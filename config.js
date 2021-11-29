0232895911f3bd6f18ecb511ab4617fedbabbdab
edangelo2 <enrique.dangelo@gmail.com>
2021-11-28 20:37:38 -0600

    populating data

--- config.js
+++ config.js
@@ -1,3 +1,18 @@
+// Last Production Deploy - Ropsten
+export const auditAssignments = "0x98E23D71eE4BC11b3eEa5feD1E428A335c0270A8"
+export const auditEnrollments = "0x6b27bFEF338102D0c36CB414bf2B26C7f477041E"
+export const DAuditaddress = "0xfbfEF368f5Ce319935D8F5429C90B380E9F9d041"
+export const auditItemAddress = "0x58485c810ccD54B9994aABD8e5b0c37796bb2BC5"
+export const auditResultAddress = "0x17E440C6c4206A089983bAcBd9cB886A053E0939"
+
+
+// Test with local contracts 
+    // export const auditAssignments = "0xA823FA2A617AF19102CaB71f5BfB918c1069A84B"
+    // export const auditEnrollments = "0xCd5cEb5Fa328630813cCeE8a181a062D0fC7c499"
+    // export const auditResultAddress = "0xA32aB87811A87c99FDc67D22F2d331DCD1f94D56"
+    // export const auditItemAddress = "0xfaDBeA8309B5610b7088ecda02d1B37706865C70"
+    // export const DAuditaddress = "0xDf3beE0F3D709e6F525dF644AF90634BbfF92B78"
+
 // Production previous
 // const auditEnrollments = "0x7364B83Df2FB101b69c2137790411Cb5e011262d"
 // const auditAssignments = "0xf5426Bf7cbE8F19E8390F68bb4D033d45e012855"
@@ -5,16 +20,9 @@
 // const auditItemAddress = "0x6345423b2869ed0367D60078930c45E91cb9013D"
 // const auditResultAddress = "0x98BcDAe1fE42ee106DF1A6aa4221363928a86aE2"
 
-// Previous
+// Previous Test
 // export const auditAssignments = "0x0b36a832cAD66ebfD7592Db4542fD42812F5d5DB"
 // export const auditEnrollments = "0xF65daDa0A2f7830dC3938A947866675f40A84239"
 // export const auditResultAddress = "0x6b27bFEF338102D0c36CB414bf2B26C7f477041E"
 // export const auditItemAddress = "0x98E23D71eE4BC11b3eEa5feD1E428A335c0270A8"
 // export const DAuditaddress = "0x05195161307E4cd0dbB3EA5c72e64A0B14418cD0"
-
-// Local
-    export const auditAssignments = "0xA823FA2A617AF19102CaB71f5BfB918c1069A84B"
-    export const auditEnrollments = "0xCd5cEb5Fa328630813cCeE8a181a062D0fC7c499"
-    export const auditResultAddress = "0xA32aB87811A87c99FDc67D22F2d331DCD1f94D56"
-    export const auditItemAddress = "0xfaDBeA8309B5610b7088ecda02d1B37706865C70"
-    export const DAuditaddress = "0xDf3beE0F3D709e6F525dF644AF90634BbfF92B78"
